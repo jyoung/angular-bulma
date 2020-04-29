d24150cfdfa9fd95ad42ea0844819f0c8d3a9e6c
Joe Young <joe.young@expresspros.com>
2020-04-29 09:04:13 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee, faCheck, faEnvelope, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-bulma';
+  coffeeIcon = faCoffee;
+  checkIcon = faCheck;
+  envelopeIcon = faEnvelope;
+  exclamationIcon = faExclamationTriangle;
 }
